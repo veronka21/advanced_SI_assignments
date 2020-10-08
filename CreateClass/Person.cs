80591e89a383958363f8ee15919091930b87950b
unknown <roni9764@gmail.com>
2020-10-08 12:06:46 +0200

    Add CreateClass project, complete 1st exercise (create Person class)

---
+++ CreateClass/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass {
+    class Person {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        Genders gender;
+
+        public Person(string name, DateTime birthDate, Genders gender) {
+            Name = name;
+            BirthDate = birthDate;
+            this.gender = gender;
+        }
+
+        public override string ToString() {
+            return $"Person: {Name}, gender: {gender}, birth date: {BirthDate.ToString("yyyy-MM-dd")}";
+        }
+    }
+
+    enum Genders {
+        Male, Female
+    }
+}
---
+++ CreateClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass {
+    class Program {
+        static void Main(string[] args) {
+            Person girl = new Person("Sophie", new DateTime(2000, 9, 21), Genders.Female);
+            Person boy = new Person("Bobbie", new DateTime(1998, 07, 21), Genders.Male);
+            Console.WriteLine(girl.ToString());
+            Console.WriteLine(boy.ToString());
+        }
+    }
+}
