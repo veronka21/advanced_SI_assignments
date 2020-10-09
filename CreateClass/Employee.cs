ac46f24d3ab15c2a290285ae7137880db43d9528
Veronika Takács <roni9764@gmail.com>
2020-10-09 14:06:51 +0200

    Complete 3rd exercise (Clone wars)

--- CreateClass/Employee.cs
+++ CreateClass/Employee.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CreateClass {
-    class Employee : Person {
+    class Employee : Person, ICloneable {
         public double Salary { get; set; }
         public string Profession { get; set; }
         public Room Room { get; set; }
@@ -23,9 +23,20 @@ namespace CreateClass {
         public override string ToString() {
             return $"{base.ToString()}, salary: {Salary}, profession: {Profession}, room: {getRoomNumber()}";
         }
+
+        public object Clone() {
+            Employee newEmployee = (Employee)this.MemberwiseClone();
+            newEmployee.Room = new Room(Room.RoomNumber);
+            return newEmployee;
+            //return this.MemberwiseClone();
+        }
     }
 
     class Room {
         public int RoomNumber { get; set; }
+
+        public Room(int roomNumber) {
+            RoomNumber = roomNumber;
+        }
     }
 }
--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -12,10 +12,17 @@ namespace CreateClass {
             Console.WriteLine(girl.ToString());
             Console.WriteLine(boy.ToString());
             Employee man = new Employee("John", new DateTime(1960, 1, 21), Genders.Male, 10000, "accountant");
-            Room room = new Room();
-            room.RoomNumber = 21;
+            Room room = new Room(21);
             man.Room = room;
             Console.WriteLine(man.ToString());
+
+            Employee Kovacs = new Employee("Géza", DateTime.Now, Genders.Male, 1000, "léhűtő");
+            Kovacs.Room = new Room(111);
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.Room.RoomNumber = 112;
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
+            Console.ReadKey();
         }
     }
 }
