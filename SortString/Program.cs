7112061d59af76bea27ca45676f516d882b114e1
Veronika Takács <roni9764@gmail.com>
2020-10-09 15:38:30 +0200

    Add "SortString" exercise

---
+++ SortString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString {
+    class Program {
+        static void Main(string[] args) {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            Array.Sort(words);
+            string s = string.Join(" ",  words);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
