0bbf0c55c0c6366b213ad47e87f4e53577db04fc
Veronika Takács <roni9764@gmail.com>
2020-10-10 12:58:58 +0200

    Add Wpf basic tutorial completed exercise

---
+++ WPF_basics_tutorial/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_basics_tutorial {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show($"The description is: {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e) {
+            this.AssemblyCheckbox.IsChecked = this.WeldCheckbox.IsChecked = this.LaserCheckbox.IsChecked = this.SawCheckbox.IsChecked = this.PlasmaCheckbox.IsChecked =
+                this.FoldCheckbox.IsChecked = this.PurchaseCheckbox.IsChecked = DrillCheckbox.IsChecked = this.LatheCheckbox.IsChecked = RollCheckbox.IsChecked = false; 
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e) {
+            LengthText.Text += ((CheckBox)sender).Content;
+        }
+
+        private void FinishDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (NoteText == null) 
+                return;
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            NoteText.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            FinishDropdown_SelectionChanged(FinishDropdown, null);
+        }
+
+        private void SupplierNameText_TextChanged(object sender, TextChangedEventArgs e) {
+            MassText.Text = SupplierNameText.Text;
+        }
+    }
+}
