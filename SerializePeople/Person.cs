9ac285a95c987733c5b2fc5d25bb4eaea3460d33
Veronika Takács <roni9764@gmail.com>
2020-10-19 21:33:31 +0200

    Make Person class serializeable

--- SerializePeople/Person.cs
+++ SerializePeople/Person.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace SerializePeople {
+    [Serializable()]
     class Person {
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
