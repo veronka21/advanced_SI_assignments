fee78ba153f420ec922cf1c0b59c89a299e4eaa2
Veronika Takács <roni9764@gmail.com>
2020-10-22 00:20:15 +0200

    Add SeekAndArchive project, implement file searching method

---
+++ SeekAndArchive/SeekAndArchive/FileSearchInDirectory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class FileSearchInDirectory
+    {
+        public static HashSet<string> Search(string fileName, string dirName)
+        {
+            Queue<string> dirsToSearch = new Queue<string>();
+            HashSet<string> foundFiles = new HashSet<string>();
+            dirsToSearch.Enqueue(dirName);
+
+            while (dirsToSearch.Count != 0)
+            {
+                string currentlySearchedDir = dirsToSearch.Dequeue();
+                DirectoryInfo directoryInfo = new DirectoryInfo(currentlySearchedDir);
+                DirectoryInfo[] directories;
+                try
+                {
+                    directories = directoryInfo.GetDirectories();
+                    AddDirectoriesToQueue(directories, ref dirsToSearch);
+                    var files = directoryInfo.GetFiles();
+                    SearchForGivenFile(files, fileName, ref foundFiles);
+                } catch(UnauthorizedAccessException e)
+                {
+                    //Console.WriteLine(e);
+                }
+                catch(DirectoryNotFoundException e)
+                {
+
+                }
+                catch(Exception e) { 
+                }
+            }
+
+            return foundFiles;
+        }
+
+        static void AddDirectoriesToQueue(DirectoryInfo[] directories, ref Queue<string> dirsToSearch)
+        {
+            foreach (var dir in directories)
+            {
+                dirsToSearch.Enqueue(dir.FullName);
+            }
+        }
+
+        static void SearchForGivenFile(FileInfo[] files, string searchedFileName, ref HashSet<string> foundFiles)
+        {
+            foreach (var file in files)
+            {
+                if (file.Name.Equals(searchedFileName))
+                {
+                    foundFiles.Add(file.FullName);
+                }
+            }
+        }
+    }
+}
---
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        public static void runSearch(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Two command arguments should be given when running the program: \n" +
+                    "1.: the searched file's name\n" +
+                    "2.: the directory to be searched");
+                Console.ReadKey();
+            } else
+            {
+                var files = FileSearchInDirectory.Search(args[0], args[1]);
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+                Console.WriteLine("End of task");
+                Console.ReadKey();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            runSearch(args);
+        }
+    }
+}
