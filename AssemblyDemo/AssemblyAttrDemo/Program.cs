eef78f23aa14af2f8710623378889f504d7b3fbd
Veronika Takács <roni9764@gmail.com>
2020-12-28 19:34:21 +0100

    Add AssemblyAttrDemo project, set AssemblyDescription attribute (exercise 2)

---
+++ AssemblyDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            Type attributeType = typeof(AssemblyDescriptionAttribute);
+            object[] customAttributes = currentAssembly.GetCustomAttributes(attributeType, false);
+            if (customAttributes.Length > 0)
+            {
+                AssemblyDescriptionAttribute assemblyDescriptionAttribute = (AssemblyDescriptionAttribute)customAttributes[0];
+                Console.WriteLine("AssemblyDescriptionAttribute description: " + assemblyDescriptionAttribute.Description);
+                Console.Read();
+            }
+        }
+    }
+}
