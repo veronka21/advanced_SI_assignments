28f79ad2de484760c5eb034ad5dda2602850abc0
Veronika Takács <roni9764@gmail.com>
2020-12-28 21:42:34 +0100

    Add DynamicCodeDemo project, complete exercise 4

---
+++ AssemblyDemo/DynamicCodeDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DynamicCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Web.dll";
+            Assembly assembly = Assembly.LoadFile(path);
+            Type type = assembly.GetType("System.Web.HttpUtility");
+            MethodInfo encode = type.GetMethod("HtmlEncode", new Type[] { typeof(string) });
+            MethodInfo decode = type.GetMethod("HtmlDecode", new Type[] { typeof(string) });
+            string toBeEncoded = "New <string> to be encoded";
+            Console.WriteLine(toBeEncoded);
+            string encoded = (string)encode.Invoke(null, new object[] { toBeEncoded });
+            Console.WriteLine(encoded);
+            string decoded = (string)decode.Invoke(null, new object[] { encoded });
+            Console.WriteLine(decoded);
+            Console.ReadKey();
+        }
+    }
+}
